7fa0b421bde568ec5bedf9dd62fe8dba555e8c5a
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2024-04-02 21:40:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,3 @@
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
