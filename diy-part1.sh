8806a491234d358a74c0e357a80e33ffdfb5b4c0
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2024-04-05 20:18:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,3 @@
 
 echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 
-rm -rf feeds/package/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb,mwan3,ddns-go,passwall}
