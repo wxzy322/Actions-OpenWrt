3d8f755b52d5eecd0b446d4a876adc346a9e29ce
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2024-04-13 12:03:13 +0800

    Create diy-part4.sh

---
+++ diy-part4.sh
@@ -0,0 +1,7 @@
+# 删除原有软件
+rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-ddns-go,luci-app-mwan3} && rm -rf feeds/packages/net/{passwall,ddns-go,mwan3}
+# 删除有可能冲突
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+# 拉取最新版golang编译
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
