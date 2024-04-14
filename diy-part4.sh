d9db2f5a650f18bc04549ea5b6555d076577a51a
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2024-04-14 18:05:56 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -16,6 +16,8 @@ git clone https://github.com/jeessy2/ddns-go.git package/feeds/smpackage/ddns-go
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/feeds/smpackage/passwall
 
 # 修改后命令
-git clone https://github.com/kenzok8/small-package --depth 1 --single-branch -b main package/feeds/smpackage/mwan3
-git clone https://github.com/kenzok8/small-package --depth 1 --single-branch -b main package/feeds/smpackage/dnsmasq
+# git clone https://github.com/kenzok8/small-package --depth 1 --single-branch -b main package/feeds/smpackage/mwan3 还是整个目录 不行
+# git clone https://github.com/kenzok8/small-package --depth 1 --single-branch -b main package/feeds/smpackage/dnsmasq
 
+svn co https://github.com/kenzok8/small-package/trunk/mwan3 package/feeds/smpackage/mwan3
+svn co https://github.com/kenzok8/small-package/trunk/dnsmasq package/feeds/smpackage/dnsmasq
