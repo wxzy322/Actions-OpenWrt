3c59c8580115a3a116c51580c22c34eced9b5d4a
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2024-04-14 18:20:05 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -19,5 +19,5 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/feeds/smpack
 # git clone https://github.com/kenzok8/small-package --depth 1 --single-branch -b main package/feeds/smpackage/mwan3 还是整个目录 不行
 # git clone https://github.com/kenzok8/small-package --depth 1 --single-branch -b main package/feeds/smpackage/dnsmasq
 
-svn co https://github.com/kenzok8/small-package/mwan3 package/feeds/smpackage/mwan3
-svn co https://github.com/kenzok8/small-package/dnsmasq package/feeds/smpackage/dnsmasq
+svn co https://github.com/kenzok8/small-package/tree/main/mwan3 package/feeds/smpackage/mwan3
+svn co https://github.com/kenzok8/small-package/tree/main/dnsmasq package/feeds/smpackage/dnsmasq
