43a1ff2beb54d5e7ba89e54bb8b6a237cb6aa48d
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2024-04-14 15:53:02 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -1,7 +1,18 @@
 # 删除原有软件
-# rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-ddns-go,luci-app-mwan3,luci-app-dnsmasq} && rm -rf feeds/packages/net/{passwall,ddns-go,mwan3,dnsmasq}
+rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-ddns-go,luci-app-mwan3,luci-app-dnsmasq} && rm -rf feeds/packages/net/{passwall,ddns-go,mwan3,dnsmasq}
 # 删除有可能冲突
 # rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
 # 拉取最新版golang编译
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+# 
+# 举例2，用第三方软件包替换当前源码库中的已有的同名软件包
+# 第一步，在 diy-part2.sh 里加入以下代码：用第一行代码先删除源码库中原来的软件，再用第二行代码引入第三方的同名软件包。
+# rm -rf package/lean/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# 
+git clone https://github.com/jeessy2/ddns-go.git package/feeds/smpackage/ddns-go
+git clone https://github.com/xiaorouji/openwrt-passwall/tree/main/luci-app-passwall.git package/feeds/smpackage/passwall
+git clone https://github.com/kenzok8/small-package/tree/main/mwan3.git package/feeds/smpackage/mwan3
+git clone https://github.com/kenzok8/small-package/tree/main/dnsmasq.git package/feeds/smpackage/dnsmasq
