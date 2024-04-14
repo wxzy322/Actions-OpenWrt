19b36328aca98807078d3a3bb8ab85f228b027f4
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2024-04-14 18:48:10 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -1,5 +1,5 @@
 # 删除原有软件
-rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-ddns-go,luci-app-mwan3,luci-app-dnsmasq} && rm -rf feeds/packages/net/{passwall,ddns-go,mwan3,dnsmasq}
+rm -rf feeds/luci/applications/{,luci-app-ddns-go,luci-app-mwan3,luci-app-dnsmasq} && rm -rf feeds/packages/net/{ddns-go,mwan3,dnsmasq}
 # 删除有可能冲突
 # rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
 # 拉取最新版golang编译
@@ -16,7 +16,7 @@ git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 # git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 # 
 git clone https://github.com/jeessy2/ddns-go.git package/feeds/smpackage/ddns-go
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/feeds/smpackage/passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall.git package/feeds/smpackage/passwall
 
 # 修改后命令
 # git clone https://github.com/kenzok8/small-package --depth 1 --single-branch -b main package/feeds/smpackage/mwan3 还是整个目录 不行
