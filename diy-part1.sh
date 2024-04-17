cbf65b7b29167a59e146ce4394541c6f9efa9edd
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2024-04-17 11:18:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,6 @@
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
-# sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '2i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '3i src-git small https://github.com/kenzok8/small' feeds.conf.default
