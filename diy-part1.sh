c9098fe4e57423226ce96b3d4f75f8732e258b09
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2024-07-11 21:35:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 #echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 # sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
