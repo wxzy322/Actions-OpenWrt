afb779b68f2690165686ce639a483bfa60e630ed
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2024-07-11 21:05:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+#echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 # sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # sed -i '2i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '3i src-git small https://github.com/kenzok8/small' feeds.conf.default
@@ -24,3 +24,5 @@ echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.d
 
 # 活动连接数修改
 sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
