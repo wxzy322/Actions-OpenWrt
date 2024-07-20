e62d471ffa4676ea4063545f1e892d0a756f76a8
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2024-07-20 16:42:43 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,3 +1,5 @@
+
+sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 # 删除&替换immortal面板及部分冲突默认软件
 
 rm -rf feeds/luci/modules/luci-base
@@ -7,3 +9,10 @@ rm -rf feeds/packages/net/v2ray-geodata
 
 svn export https://github.com/immortalwrt/luci/branches/master/modules/luci-base feeds/luci/modules/luci-base
 svn export https://github.com/immortalwrt/luci/branches/master/modules/luci-mod-status feeds/luci/modules/luci-mod-status
+
+
+rm -rf ./tmp
+
+./scripts/feeds update -a
+
+./scripts/feeds install -a
