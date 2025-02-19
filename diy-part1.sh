d281843a93dc6ced37d7435f7233e283eb214a52
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2025-02-19 11:32:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
@@ -19,8 +5,8 @@
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
 
-#echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
-#sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+# sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # sed -i '2i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '3i src-git small https://github.com/kenzok8/small' feeds.conf.default
 # echo 'src-git roacn https://github.com/roacn/openwrt-packages' >>feeds.conf.default
@@ -29,14 +15,14 @@
 
 # 活动连接数修改
 # sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
-#6.6连接数
-sed -i '/customized in this file/a fs.file-max=102400\nnet.ipv4.neigh.default.gc_thresh1=512\nnet.ipv4.neigh.default.gc_thresh2=2048\nnet.ipv4.neigh.default.gc_thresh3=4096\nnet.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+# 6.6连接数
+# sed -i '/customized in this file/a fs.file-max=102400\nnet.ipv4.neigh.default.gc_thresh1=512\nnet.ipv4.neigh.default.gc_thresh2=2048\nnet.ipv4.neigh.default.gc_thresh3=4096\nnet.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 # 6.6修正连接数（by ベ七秒鱼ベ）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
 
 
-#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 
 # 添加额外插件
